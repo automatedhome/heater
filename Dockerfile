62205494e2b7e5417fe4c833a732e259a3474864
paulfantom <pawel@krupa.net.pl>
2019-08-27 15:51:48 +0200

    use config file

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM arm32v7/golang:stretch
+FROM arm32v7/golang:stretch as builder
 
 COPY qemu-arm-static /usr/bin/
 WORKDIR /go/src/github.com/automatedhome/heater
@@ -7,6 +7,7 @@ RUN make build
 
 FROM arm32v7/busybox:1.30-glibc
 
-COPY --from=0 /go/src/github.com/automatedhome/heater/heater /usr/bin/heater
+COPY --from=builder /go/src/github.com/automatedhome/heater/heater /usr/bin/heater
+COPY --from=builder /go/src/github.com/automatedhome/heater/config.yaml /config.yaml
 
 ENTRYPOINT [ "/usr/bin/heater" ]
--- cmd/main.go
+++ cmd/main.go
@@ -2,50 +2,57 @@ package main
 
 import (
 	"flag"
+	"io/ioutil"
 	"log"
 	"net/url"
 	"strings"
 	"strconv"
 	"time"
 
+	"gopkg.in/yaml.v2"
+
 	mqttclient "github.com/automatedhome/common/pkg/mqttclient"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
-type BoolPoint struct {
-	v    bool
-	addr string
-}
-
 type DataPoint struct {
-	v    float64
-	addr string
+	Value   float64 `yaml:"value,omitempty"`
+	Address string  `yaml:"address"`
 }
 
 type Settings struct {
-	tankMin        DataPoint
-	tankMax        DataPoint
-	heaterCritical DataPoint
-	hysteresis     DataPoint
-	expected       DataPoint
+	TankMin        DataPoint `yaml:"tankMin"`
+	TankMax        DataPoint `yaml:"tankMax"`
+	HeaterCritical DataPoint `yaml:"heaterCritical"`
+	Hysteresis     DataPoint `yaml:"hysteresis"`
+	Expected       DataPoint `yaml:"expected"`
 }
 
 type Sensors struct {
-	roomTemp  DataPoint
-	tankUp    DataPoint
-	heaterIn  DataPoint
-	heaterOut DataPoint
+	HeaterIn  DataPoint `yaml:"heaterIn"`
+	HeaterOut DataPoint `yaml:"heaterOut"`
+	RoomTemp  DataPoint `yaml:"roomTemp"`
+	TankUp    DataPoint `yaml:"tankUp"`
 }
 
 type Actuators struct {
-	heater BoolPoint
-	sw     BoolPoint //CH == False, DHW == True
+	Heater string `yaml:"heater"`
+	Switch string `yaml:"switch"` //CH == False, DHW == True
 }
 
+type Config struct {
+	Actuators Actuators `yaml:"actuators"`
+	Sensors   Sensors   `yaml:"sensors"`
+	Settings  Settings  `yaml:"settings"`
+}
+
+var config Config
 var settings Settings
 var sensors Sensors
 var actuators Actuators
 var client mqtt.Client
+var heaterState bool
+var switchState bool
 
 func onMessage(client mqtt.Client, message mqtt.Message) {
 	value, err := strconv.ParseFloat(string(message.Payload()), 64)
@@ -54,41 +61,41 @@ func onMessage(client mqtt.Client, message mqtt.Message) {
 	}
 
 	switch message.Topic() {
-	case sensors.roomTemp.addr:
-		sensors.roomTemp.v = value
-	case sensors.tankUp.addr:
-		sensors.tankUp.v = value
-	case sensors.heaterIn.addr:
-		sensors.heaterIn.v = value
-	case sensors.heaterOut.addr:
-		sensors.heaterOut.v = value
-	case settings.tankMin.addr:
-		settings.tankMin.v = value
-	case settings.tankMax.addr:
-		settings.tankMax.v = value
-	case settings.heaterCritical.addr:
-		settings.heaterCritical.v = value
-	case settings.expected.addr:
-		settings.expected.v = value
-	case settings.hysteresis.addr:
-		settings.hysteresis.v = value
+	case sensors.RoomTemp.Address:
+		sensors.RoomTemp.Value = value
+	case sensors.TankUp.Address:
+		sensors.TankUp.Value = value
+	case sensors.HeaterIn.Address:
+		sensors.HeaterIn.Value = value
+	case sensors.HeaterOut.Address:
+		sensors.HeaterOut.Value = value
+	case settings.TankMin.Address:
+		settings.TankMin.Value = value
+	case settings.TankMax.Address:
+		settings.TankMax.Value = value
+	case settings.HeaterCritical.Address:
+		settings.HeaterCritical.Value = value
+	case settings.Expected.Address:
+		settings.Expected.Value = value
+	case settings.Hysteresis.Address:
+		settings.Hysteresis.Value = value
 	}
 }
 
 func heater(state bool, reason string) {
-	if state == actuators.heater.v {
+	if state == heaterState {
 		return
 	}
 
-	actuators.heater.v = state
+	heaterState = state
 	if state {
 		log.Println("Starting: " + reason)
-		client.Publish(actuators.heater.addr, 0, false, "1")
+		client.Publish(actuators.Heater, 0, false, "1")
 		return
 	}
 
 	log.Println("Stopping: " + reason)
-	client.Publish(actuators.heater.addr, 0, false, "0")
+	client.Publish(actuators.Heater, 0, false, "0")
 }
 
 func sw(destination string) {
@@ -96,69 +103,83 @@ func sw(destination string) {
 	if destination == "water" {
 		state = true
 	}
-	if state == actuators.heater.v {
+	if state == heaterState {
 		return
 	}
 
-	actuators.sw.v = state
+	switchState = state
 	if state {
 		log.Println("Switching actuator in water heating position")
-		client.Publish(actuators.sw.addr, 0, false, "1")
+		client.Publish(actuators.Switch, 0, false, "1")
 		return
 	}
 
 	log.Println("Switching actuator in home heating position")
-	client.Publish(actuators.sw.addr, 0, false, "0")
+	client.Publish(actuators.Switch, 0, false, "0")
 }
 
 func init() {
-	// TODO read it from yaml file
-	settings = Settings{}
-	sensors = Sensors{}
-	actuators = Actuators{}
-	actuators.heater = BoolPoint{false, "heater/actuators/burner"} // proxy to "evok/relay/5/set"
-	actuators.sw = BoolPoint{false, "heater/actuators/switch"}     // proxy to "evok/relay/1/set"
-
-	sensors.roomTemp = DataPoint{300, "climate/temperature/inside"}
-	sensors.tankUp = DataPoint{300, "tank/temperature/up"}
-	sensors.heaterIn = DataPoint{300, "heater/temperature/in"}
-	sensors.heaterOut = DataPoint{300, "heater/temperature/out"}
-
-	settings.heaterCritical = DataPoint{80, "heater/settings/critical"}
-	settings.hysteresis = DataPoint{1, "heater/settings/hysteresis"}
-	settings.expected = DataPoint{18, "heater/settings/expected"}
-	settings.tankMin = DataPoint{45, "heater/settings/tankmin"}
-	settings.tankMax = DataPoint{55, "heater/settings/tankmax"}
+	heaterState = false
+	switchState = false
 }
 
 func main() {
 	broker := flag.String("broker", "tcp://127.0.0.1:1883", "The full url of the MQTT server to connect to ex: tcp://127.0.0.1:1883")
 	clientID := flag.String("clientid", "heater", "A clientid for the connection")
+	configFile := flag.String("config", "/config.yaml", "Provide configuration file with MQTT topic mappings")
 	flag.Parse()
 
-	brokerURL, _ := url.Parse(*broker)
+	brokerURL, err := url.Parse(*broker)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Reading configuration from %s", *configFile)
+	data, err := ioutil.ReadFile(*configFile)
+	if err != nil {
+		log.Fatalf("File reading error: %v", err)
+		return
+	}
+
+	err = yaml.UnmarshalStrict(data, &config)
+	//err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	settings = config.Settings
+	actuators = config.Actuators
+	sensors = config.Sensors
+	// set initial sensors values and ignore ones provided by config file
+	// this is used as a locking mechanism to prevent starting control loop without current sensors data
+	lockTemp := 300.0
+	sensors.HeaterIn.Value = lockTemp
+	sensors.HeaterOut.Value = lockTemp
+	sensors.RoomTemp.Value = lockTemp
+	sensors.TankUp.Value = lockTemp
+
 	var topics []string
-	topics = append(topics, sensors.heaterIn.addr, sensors.heaterOut.addr, sensors.tankUp.addr, sensors.roomTemp.addr)
-	topics = append(topics, settings.heaterCritical.addr, settings.hysteresis.addr, settings.tankMin.addr, settings.tankMax.addr, settings.expected.addr)
+	topics = append(topics, sensors.HeaterIn.Address, sensors.HeaterOut.Address, sensors.TankUp.Address, sensors.RoomTemp.Address)
+	topics = append(topics, settings.HeaterCritical.Address, settings.Hysteresis.Address, settings.TankMin.Address, settings.TankMax.Address, settings.Expected.Address)
 	client = mqttclient.New(*clientID, brokerURL, topics, onMessage)
 	log.Printf("Connected to %s as %s and waiting for messages\n", *broker, *clientID)
 
 	msg := []string{"Waiting 15s for sensors data. Currently lacking:"}
 	// Wait for sensors data
 	for {
-		if sensors.roomTemp.v != 0 && sensors.tankUp.v != 0 && sensors.heaterIn.v != 0 && sensors.heaterOut.v != 0 {
+		if sensors.RoomTemp.Value != 0 && sensors.TankUp.Value != 0 && sensors.HeaterIn.Value != 0 && sensors.HeaterOut.Value != 0 {
 			break
 		}
-		if sensors.heaterIn.v == 300 {
+		if sensors.HeaterIn.Value == 300 {
 			msg = append(msg, "heaterIn")
 		}
-		if sensors.heaterOut.v == 300 {
+		if sensors.HeaterOut.Value == 300 {
 			msg = append(msg, "heaterOut")
 		}
-		if sensors.roomTemp.v == 300 {
+		if sensors.RoomTemp.Value == 300 {
 			msg = append(msg, "roomTemp")
 		}
-		if sensors.tankUp.v == 300 {
+		if sensors.TankUp.Value == 300 {
 			msg = append(msg, "tankUp")
 		}
 		log.Println(strings.Join(msg, " "))
@@ -172,30 +193,30 @@ func main() {
 		time.Sleep(1 * time.Second)
 
 		// failsafe
-		if sensors.heaterOut.v >= settings.heaterCritical.v {
+		if sensors.HeaterOut.Value >= settings.HeaterCritical.Value {
 			heater(false, "critical heater temperature reached")
 			continue
 		}
 
 		// Water heating start
-		if sensors.tankUp.v < settings.tankMin.v {
+		if sensors.TankUp.Value < settings.TankMin.Value {
 			heater(true, "water heating")
 			sw("water")
 			continue
 		}
 
 		// water heating ends
-		if sensors.tankUp.v >= settings.tankMax.v {
+		if sensors.TankUp.Value >= settings.TankMax.Value {
 			sw("room")
 		}
 
 		// room heating
-		if sensors.roomTemp.v < settings.expected.v-settings.hysteresis.v/2 {
+		if sensors.RoomTemp.Value < settings.Expected.Value-settings.Hysteresis.Value/2 {
 			heater(true, "room temperature lower than expected")
 			continue
 		}
 
-		if sensors.roomTemp.v > settings.expected.v+settings.hysteresis.v/2 {
+		if sensors.RoomTemp.Value > settings.Expected.Value+settings.Hysteresis.Value/2 {
 			heater(false, "expected room temperature achieved")
 		}
 	}
